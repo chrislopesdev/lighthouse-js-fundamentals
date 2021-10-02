53f2e729e6404e5fbbecbe3552baaffc9717d5ae
chris lopes <chris.lopes@gmail.com>
2021-10-02 11:39:45 +0000

    updated spacing

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,6 +1,6 @@
 for (let i = 100; i <= 200; i++) {
   if ((i % 3 === 0) && (i % 4 === 0)) {
-  console.log("LoopyLighthouse");
+    console.log("LoopyLighthouse");
   } else if (i % 4 === 0) {
     console.log("Lighthouse");
   } else if (i % 3 === 0 ) {
