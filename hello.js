7089b3914874e75d880474f8ddf29c5601520fc8
chris lopes <chris.lopes@gmail.com>
2021-10-02 17:42:18 +0000

    more function work

--- hello.js
+++ hello.js
@@ -8,5 +8,29 @@
 const sayHello = function (name) {
   console.log(`Hello, ${name}`);
 }
-
 sayHello("Chris");
+
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
+
+
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+// Can be written without creating variables:
+console.log(isEven(10));
+console.log(isEven(11));
