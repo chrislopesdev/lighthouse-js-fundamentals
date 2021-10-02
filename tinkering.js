7099b268243d0a07c14f117766daf9832a86eb2f
chris lopes <chris.lopes@gmail.com>
2021-10-02 16:57:02 +0000

    added code working with functions

--- tinkering.js
+++ tinkering.js
@@ -5,3 +5,43 @@ function multiply(a, b) {
 const result = multiply(2, 4);
 
 console.log(result);
+
+
+function reverseString(reverseMe) {
+  let reversed = "";
+  for (let i = reverseMe.length - 1; i >= 0; i--) {
+    reversed += reverseMe[i];
+  }
+  return reversed;
+}
+
+console.log(reverseString("christopher"));
+
+
+function sayHello() {
+  const message = "Hello!";
+  return message;
+}
+
+console.log(sayHello());
+
+function findAverage(x, y) {
+  let answer = (x + y) / 2;
+  return answer;
+}
+
+console.log(findAverage(5, 9));
+
+
+function isPrime(integer) {
+  for (let x = 2; x < integer; x++) {
+    if(integer % x === 0) {
+      console.log(integer + " is divisible by " + x);
+      return false;
+    }
+  }
+  console.log(integer + " is a prime number.")
+  return true;
+}
+
+isPrime(47);
