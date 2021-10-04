3914498d8a126e93b3a8a1a1531ea95c3ba309be
chris lopes <chris.lopes@gmail.com>
2021-10-04 11:15:43 +0000

    init commit icecream assignment

---
+++ arrays-icecream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// add root beer
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// get first flavour
+console.log(iceCreamFlavours[0]);
+
+// get last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+// how many flavours are available
+console.log(iceCreamFlavours.length);
